ab782f9cdfbcfbbcdb606bfb10cd97af907cc2e7
IDO <susususu07@naver.com>
2024-03-03 05:34:02 +0900

    Add files via upload
    클래스와 메서드

---
+++ java-basic/src/variable/ValueDataMain2.java
@@ -0,0 +1,14 @@
+package oop1;
+
+public class ValueDataMain2 { // 1. 객제치향 프로그래밍
+
+
+    public static void main(String[] args) {
+        ValueData2 valueData = new ValueData2();
+        valueData.add();
+        valueData.add();
+        valueData.add();
+        System.out.println("최종 숫자=" + valueData.value);
+    }
+
+}
