ea148994a9e09c30c9e4740fa98eac0ec33cc499
IDO <susususu07@naver.com>
2024-03-04 03:46:16 +0900

    Update FinalRefMain.java

--- java-basic/src/final1/FinalRefMain.java
+++ java-basic/src/final1/FinalRefMain.java
@@ -3,7 +3,7 @@ package final1;
 public class FinalRefMain {
     public static void main(String[] args) {
         final Data data = new Data();
-        //data = new Data();
+        //data = new Data(); // 한 번 참조값을 담았기에 컴파일 오류
 
         //참조 대상의 값은 변경 가능
         data.value = 10;
