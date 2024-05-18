034a2bfbfd38f0b6377e66b60ef79f6f90b2421e
IDO <susususu07@naver.com>
2024-05-18 19:54:56 +0900

    Update Data3.java

--- java-basic/src/static1/Data3.java
+++ java-basic/src/static1/Data3.java
@@ -1,8 +1,8 @@
 package static1;
 
 public class Data3 {
-    public String name;
-    public static int count; // static
+    public String name; // 인스턴스 변수
+    public static int count; // static 변수 (= 정적 변수, 클래스 변수)
 
     public Data3(String name) {
         this.name = name;
